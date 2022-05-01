9534e00e22cb06ae89b35e2413d3e8a2fb541950
zhaodeezhu <zhaodeezhu@126.com>
2022-05-01 17:10:10 +0800

    fix: 🐛 修改未打包的问题

--- plugins/Mysql/src/query.ts
+++ plugins/Mysql/src/query.ts
@@ -190,7 +190,7 @@ class Query {
         sql += `update ${table}
                   set ${item[0]} = case ${mainkey}`
         item.forEach((word, index) => {
-          const finalWord = word === null || typeof word === 'boolean' ? word : `'${word}'`
+          const finalWord = word === null || typeof word === 'boolean' || typeof word === 'number' ? word : `'${word}'`
           if (index === 0) return false
           sql += ` when '${idsArr[index - 1]}' then ${finalWord}`
         })
@@ -198,7 +198,7 @@ class Query {
       } else {
         sql += `,${item[0]} = case ${mainkey}`;
         item.forEach((word, index) => {
-          const finalWord = word === null || typeof word === 'boolean' ? word : `'${word}'`
+          const finalWord = word === null || typeof word === 'boolean' || typeof word === 'number' ? word : `'${word}'`
           if (index === 0) return false
           sql += ` when '${idsArr[index - 1]}' then ${finalWord}`
         })
